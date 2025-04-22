d6781fd372588d3455e2df600d6ae708ee5cc11c
autos1123 <hea514@naver.com>
2025-04-22 15:58:19 +0900

    fix : 몬스터 수정
    - 타입별로 나눴지만 스테이지별로 몬스터를 나눴습니다.
    - 몬스터 이름은 임의로 설정했습니다.
    - 몬스터가 죽을때 경험치와 골드가 나오도록 cvs에서 수정했습니다.
    - Monster.cs에서도 cvs처럼 경험치와 골드를 추가했습니다.
    - MonsterFactory.cs 하단부 주석은 무시하셔도됩니다.

--- TextRpg/Monster.cs
+++ TextRpg/Monster.cs
@@ -7,18 +7,20 @@ using System.Threading.Tasks;
 namespace TextRpg
 {
     public class Monster
-    {
-        public string Type { get; set; }
+    { // 몬스터의 기본 스텟 받아오기
+        public string Stage { get; set; }
         public string Name { get; set; }
+        public int Level { get; set; }
         public int MaxHP { get; set; }
         public int CurrentHP { get; set; }
         public int Attack { get; set; }
         public int Defense { get; set; }
-        public int Speed { get; set; }
+        public int DropExp { get; set; }
+        public int DropGold { get; set; }
 
         public override string ToString()
-        {
-            return $"{Name} (HP: {CurrentHP}/{MaxHP}, ATK: {Attack}, DEF: {Defense}, SPD: {Speed})";
+        { // 몬스터가 생성되었을때 나오는 UI
+            return $"Lv.{Level} {Name} HP: {CurrentHP}/{MaxHP}";
         }
     }
 }

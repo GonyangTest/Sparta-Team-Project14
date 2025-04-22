ba392521edd0a4dfa84c720b94b7c3aaceed564c
Won0001 <smjwnaya1102@gmail.com>
2025-04-23 00:00:02 +0900

    refactor : 직업 선택 코드 정리
    - 직업 딕셔너리로 정리

--- TextRpg/Player.cs
+++ TextRpg/Player.cs
@@ -1,30 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TextRpg
 {
+    public class Job
+    {
+        public string Name;
+        public int Hp;
+        public int Mana;
+        public int MaxHp;
+        public int MaxMp;
+        public float Power;
+        public int Defense;
+        public int Agility;
+
+        public Job(string name, int hp, int mana, int maxHp, int maxMp, float power, int defense, int agility)
+        {
+            Name = name;
+            Hp = hp;
+            Mana = mana;
+            MaxHp = maxHp;
+            MaxMp = maxMp;
+            Power = power;
+            Defense = defense;
+            Agility = agility;
+        }
+
+        public static Dictionary<int, Job> JobList = new Dictionary<int, Job>()
+        {
+            {1, new Job("전사", 150, 80, 150, 80, 8f, 6, 0)},
+            {2, new Job("도적", 100, 100, 100, 100, 4f, 5, 0)},
+            {3, new Job("궁수", 100, 100, 100, 100, 6f, 5, 0)},
+            {4, new Job("마법사", 80, 150, 80, 150, 7f, 4, 0)}
+        };
+    }
 
     class Player
     {
-        public string playerName = ""; // 이름
-        public string playerClass = ""; // 직업정보
-        public int level = 1; // 레벨
-        public int exp = 0; // 경험치
-        public int gold = 10000; // 골드
-        public int hp = 0; // 체력
-        public float power = 0.0f; // 공격력
-        public int defense = 0; // 방어력
-        public float basePower = 0.0f;
-        public int baseDefense = 0;
-        //추가스텟
-        public int mana = 0;
-        public int agility = 0; // 민첩
-        public int maxHp = 100; // 최대 체력
-        public int maxMp = 100; // 최대 마나
-        public int maxExp = 10; // 최대 경험치
+        public string playerName;
+        public string playerClass;
+        public int level = 1;
+        public int exp = 0;
+        public int maxExp = 10;
+        public int gold = 10000;
+        public int hp;
+        public int mana;
+        public int maxHp;
+        public int maxMp;
+        public float power;
+        public int defense;
+        public int agility;
+        public Job SelectedJob;
 
         public ConsumableItem HealthPotion = new ConsumableItem("체력포션", "체력을 회복하는 포션", 50, ConsumableItem.OptionType.Health, 30, 3);
         public ConsumableItem ManaPotion = new ConsumableItem("마나포션", "마나를 회복하는 포션", 100, ConsumableItem.OptionType.Mana, 30, 3);
@@ -33,47 +63,7 @@ namespace TextRpg
         public Item EquippedArmor;
 
         // 체력 포션 사용
-        public bool UseHealthPotion()
-        {
-            if (hp >= maxHp)
-            {
-                Console.WriteLine("\n이미 최대체력입니다.");
-                return false;
-            }
-
-            return HealthPotion.Use(this);
-        }
-
-        // 마나 포션 사용
-        public bool UseManaPotion()
-        {
-            if (mana >= maxMp)
-            {
-                Console.WriteLine("\n이미 최대마나입니다.");
-                return false;
-            }
-
-            return ManaPotion.Use(this);
-        }
-
-        // 레벨업시 변화
-        public void LevelUp()
-        {
-            while (exp >= maxExp)
-            {
-                level++;
-                exp -= maxExp;
-
-                // 필요 경험치 증가량 로직 생성
-                int increase = 25 + (level - 2) * 5;
-                maxExp += increase;
-
-                // 레벨업시 공격력 0.5증가 방어력 1증가
-                basePower += 0.5f;
-                baseDefense += 1;
-            }
-
-        }
+        
 
         public void SetPlayer()
         {
@@ -86,6 +76,7 @@ namespace TextRpg
                 Console.WriteLine("원하시는 이름을 설정해주세요.");
                 string userName = Console.ReadLine();
                 playerName = userName;
+
                 while (true)
                 {
                     Console.Clear();
@@ -93,6 +84,7 @@ namespace TextRpg
                     Console.WriteLine("1.저장\n2.취소\n");
                     Console.WriteLine("원하시는 행동을 입력해주세요.");
                     string userNameSelect = Console.ReadLine();
+
                     int userNameTmp;
                     if (!int.TryParse(userNameSelect, out userNameTmp))
                     {
@@ -133,6 +125,7 @@ namespace TextRpg
                 Console.WriteLine("1.전사\n2.도적\n3.궁수\n4.마법사\n");
                 Console.WriteLine("원하시는 행동을 입력해주세요.");
                 string job = Console.ReadLine();
+
                 int selectJob;
                 if (!int.TryParse(job, out selectJob))
                 {
@@ -140,66 +133,71 @@ namespace TextRpg
                     Console.WriteLine("목록에 나온 숫자만 입력하세요.");
                     Console.ReadKey();
                 }
-                else
-                {
-                    switch (selectJob)
-                    {
-                        case 1:
-                            playerClass = "전사";
-                            hp = 150;
-                            maxHp = 150;
-                            basePower = 8f;
-                            baseDefense = 6;
-                            agility = 0;
-                            mana = 80;
-                            maxMp = 80;
-                            break;
-                        case 2:
-                            playerClass = "도적";
-                            hp = 100;
-                            maxHp = 100;
-                            basePower = 4f;
-                            baseDefense = 5;
-                            agility = 20;
-                            mana = 100;
-                            maxMp = 100;
-                            break;
-                        case 3:
-                            playerClass = "궁수";
-                            hp = 100;
-                            maxHp = 100;
-                            basePower = 6f;
-                            baseDefense = 5;
-                            agility = 10;
-                            mana = 100;
-                            maxMp = 100;
-                            break;
-                        case 4:
-                            playerClass = "마법사";
-                            hp = 80;
-                            maxHp = 85;
-                            basePower = 7f;
-                            baseDefense = 4;
-                            agility = 0;
-                            mana = 150;
-                            maxMp = 150;
-                            break;
-                        default:
-                            Console.Clear();
-                            Console.WriteLine("목록에 나온 숫자만 입력하세요.");
-                            Console.ReadKey();
-                            continue;
-                    }
-                    break;
-                }
+
+                SelectedJob = Job.JobList[selectJob];
+                playerClass = SelectedJob.Name;
+                hp = SelectedJob.Hp;
+                mana = SelectedJob.Mana;
+                maxHp = SelectedJob.MaxHp;
+                maxMp = SelectedJob.MaxMp;
+                power = SelectedJob.Power;
+                defense = SelectedJob.Defense;
+                agility = SelectedJob.Agility;
+
+                break;
+                
             }
+        }
+
+        public bool UseHealthPotion()
+        {
+            if (SelectedJob.Hp >= SelectedJob.MaxHp)
+            {
+                Console.WriteLine("\n이미 최대체력입니다.");
+                return false;
+            }
+
+            return HealthPotion.Use(this);
+        }
+
+        // 마나 포션 사용
+        public bool UseManaPotion()
+        {
+            if (SelectedJob.Mana >= SelectedJob.MaxMp)
+            {
+                Console.WriteLine("\n이미 최대마나입니다.");
+                return false;
+            }
+
+            return ManaPotion.Use(this);
+        }
+
+
+
+        // 레벨업시 변화
+        public void LevelUp()
+        {
+            while (exp >= maxExp)
+            {
+                level++;
+                exp -= maxExp;
+
+                // 필요 경험치 증가량 로직 생성
+                int increase = 25 + (level - 2) * 5;
+                maxExp += increase;
+
+                // 레벨업시 공격력 0.5증가 방어력 1증가
+                power += 0.5f;
+                defense += 1;
+            }
+
         }
         public float totalPower
         {
             get
             {
-                int weaponBonus = EquippedWeapon is Weapon w ? w.attack : 0;
-                return basePower + weaponBonus;
+                float weaponBonus = EquippedWeapon is Weapon w ? w.attack : 0;
+                return power + weaponBonus;
             }
         }
 
@@ -208,7 +206,7 @@ namespace TextRpg
             get
             {
                 int armorBonus = EquippedArmor is Armor a ? a.defense : 0;
-                return baseDefense + armorBonus;
+                return defense + armorBonus;
             }
         }
         public string CurrentPlayer()
@@ -229,7 +227,7 @@ namespace TextRpg
                 $"민첩: {agility}\n" +
                 $"장착 무기: {(EquippedWeapon != null ? EquippedWeapon.itemName : "없음")}\n" +
                 $"장착 방어구: {(EquippedArmor != null ? EquippedArmor.itemName : "없음")}\n" +
-                $"골드 : {gold}\n";
+                $"골드 : {gold}G\n";
                 Console.WriteLine("0. 나가기\n");
                 Console.WriteLine("원하시는 행동을 입력해주세요.");
                 string current = Console.ReadLine();

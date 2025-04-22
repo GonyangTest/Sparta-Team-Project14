5a0059ae9022a0831a44a74a13c443d675b2a17f
ybbro <uvinjjang@naver.com>
2025-04-22 17:46:10 +0900

    docs : player, inventory 전역변수로 변경, quest 인스턴스 추가
    1. Main() 내부의 지역변수에서 Program.cs의 전역 변수로 변경
    2. 접근제한자 internal static으로 변경
    3. 새로 만든 Quest 클래스에 대한 인스턴스를 생성
    
    사유 : 퀘스트에서 보상/달성 조건을 확인하기 위해 해당 인스턴스들에 접근할 필요가 있었습니다.

--- TextRpg/Program.cs
+++ TextRpg/Program.cs
@@ -3,20 +3,18 @@ using TextRpg;
 
 internal class Program
 {
+    internal static Player player = new Player();
+    internal static Inventory inventory = new Inventory();
+    internal static Quest quest = new Quest();
     static void Main(string[] args)
     {
-        Player player = new Player();
         Town town = new Town();
-        Inventory inventory = new Inventory();
         Shop shop = new Shop(player);
         Dungeon dungeon = new Dungeon();
         Rest rest = new Rest();
         
-
-
         player.SetPlayer();
         Console.Clear(); // 콘솔 화면 정리 (선택사항)
         town.TownMap(player, inventory, shop, dungeon, rest);
-
     }
 }

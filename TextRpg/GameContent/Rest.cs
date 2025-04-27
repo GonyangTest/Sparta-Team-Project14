e89880199703fa29bc6c50cdd15d23a59eef5d76
ybbro <uvinjjang@naver.com>
2025-04-27 22:09:01 +0900

    feat: Rest에 플레이어 체력, 마나 바 표시 (직관적으로 사용 가능한지 표시)
    .

--- TextRpg/GameContent/Rest.cs
+++ TextRpg/GameContent/Rest.cs
@@ -14,6 +14,7 @@ namespace TextRpg
             while (true)
             {
                 Console.Clear();
+                PrintPlayer(true);
                 AnsiConsole.Write(new Rule("[red]사용[/][green]할 아이템을 선택하세요[/]"));
                 Console.WriteLine("회복");
                 Console.WriteLine($"[포션 아이템]");
@@ -57,11 +58,64 @@ namespace TextRpg
                     player.UseManaPotion();
                     break;
             }
+            Console.SetCursorPosition(0,0);
+            PrintPlayer(true);
             Thread.Sleep(GameConstance.Rest.USE_POTION_SLEEP_TIME); // 포션 사용/사용불가 메세지가 출력 후 보여지는 시간이 필요하여 추가
             while (Console.KeyAvailable)
             {
                 Console.ReadKey(true); // 버퍼에 남아있는 키를 읽고 버립니다.
             }
+        }
+
+        void PrintProgressBar(string label, double current, double max, Color completedColor, Color remainingColor)
+        {
+            int barWidth = 20; // 진행바 길이
+            
+
+            string colorCode;
+            if (completedColor == Color.Red) colorCode = "red";
+            else if (completedColor == Color.Blue) colorCode = "blue";
+            else colorCode = "#6cf540";
+            AnsiConsole
+                .Progress()
+                .Columns(
+                    new TaskDescriptionColumn(),
+                    new ProgressBarColumn
+                    {
+                        Width = barWidth,
+                        CompletedStyle = new Style(foreground: completedColor),
+                        RemainingStyle = new Style(foreground: remainingColor),
+                        FinishedStyle = new Style(foreground: completedColor),
+                    })
+                .Start(x =>
+                {
+                    var task = x.AddTask($"[{colorCode}]{label}[/]");
+                    float percentage = (float)(current * 100 / max);
+                    task.Increment(percentage);
+                });
+
+            Console.SetCursorPosition(barWidth + 5, Console.GetCursorPosition().Top - 2);
+            AnsiConsole.MarkupLine($"[{colorCode}]{current}/{max}[/]" + new string(' ', max.ToString().Length));
+        }
+        void PrintPlayer(bool isWithExpBar)
+        {
+            int partician_Length = 35; // 유닛 간 나누는 줄 길이
+
+            AnsiConsole.MarkupLine($"[#6cf540]{new string('-', partician_Length)}[/]");
+            // 플레이어
+            AnsiConsole.MarkupLine($"[yellow bold]{Program.player.playerName}[/] ({Program.player.playerClass}) Lv. {Program.player.level}");
+            //// HP바
+            PrintProgressBar("HP ", Program.player.hp, Program.player.maxHp, Color.Red, Color.Red3);
+            // MP바
+            PrintProgressBar("MP ", Program.player.mana, Program.player.maxMp, Color.Blue, Color.Blue3);
+            // Exp 바도 함께 출현
+            if (isWithExpBar)
+            {
+                // 경험치 바 (초록)
+                PrintProgressBar("EXP", Program.player.exp, Program.player.maxExp, Color.FromHex("#6cf540"), Color.FromHex("#62911A"));
+            }
+            // 아래 구분선
+            AnsiConsole.MarkupLine($"[#6cf540]{new string('-', partician_Length)}[/]\n");
         }
     }
 }
